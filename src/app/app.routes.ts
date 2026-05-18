9afac410232f43f9ca74cc8ca71663dcd193f213
Keheliya-Sathsara <keheliyasm@gmail.com>
2026-05-18 14:49:59 +0530

    Software page created and added to the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { BlogsComponent } from './pages/blogs/blogs.component';
 import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { SoftwareComponent } from './pages/software/software.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'blogs', component: BlogsComponent },
   { path: 'blog/:slug', component: BlogDetailComponent },
+  { path: 'software', component: SoftwareComponent },
 ];
---
+++ src/app/pages/software/software.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-software',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './software.component.html',
+  styleUrl: './software.component.scss'
+})
+export class SoftwareComponent {
+
+}
