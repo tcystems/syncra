ab5b989f77a4dbc36be1c26a2ebf56ed0b72d9d5
Keheliya-Sathsara <keheliyasm@gmail.com>
2026-06-12 16:47:48 +0530

    changes

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -11,11 +11,52 @@ export class BlogService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Converts a Google Drive share URL to a direct embeddable image URL.
+   * Handles formats like:
+   *   https://drive.google.com/file/d/FILE_ID/view
+   *   https://drive.google.com/open?id=FILE_ID
+   *   https://drive.google.com/uc?id=FILE_ID
+   */
+  private toDirectImageUrl(url: string): string {
+    if (!url) return url;
+
+    // Already a direct/thumbnail URL
+    if (url.includes('thumbnail?id=') || url.includes('uc?export=view')) {
+      return url;
+    }
+
+    // Extract file ID from /file/d/FILE_ID/... format
+    const fileMatch = url.match(/\/file\/d\/([^/]+)/);
+    if (fileMatch) {
+      return `https://drive.google.com/thumbnail?id=${fileMatch[1]}&sz=w1000`;
+    }
+
+    // Extract file ID from ?id=FILE_ID format
+    const idMatch = url.match(/[?&]id=([^&]+)/);
+    if (idMatch) {
+      return `https://drive.google.com/thumbnail?id=${idMatch[1]}&sz=w1000`;
+    }
+
+    return url;
+  }
+
+  private transformBlog(blog: any): any {
+    if (blog && blog.imageUrl) {
+      blog.imageUrl = this.toDirectImageUrl(blog.imageUrl);
+    }
+    return blog;
+  }
+
   getBlogs(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?action=getAll`);
+    return this.http.get<any[]>(`${this.apiUrl}?action=getAll`).pipe(
+      map(blogs => blogs.map(b => this.transformBlog(b)))
+    );
   }
 
   getBlogBySlug(slug: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?action=getSingle&slug=${slug}`);
+    return this.http.get<any>(`${this.apiUrl}?action=getSingle&slug=${slug}`).pipe(
+      map(blog => this.transformBlog(blog))
+    );
   }
 }
